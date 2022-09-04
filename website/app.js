73a3f78b4e382e2f0a1cd681599bf5604673c656
Mohammed Alfyad <74600074+MohammedAlfyad@users.noreply.github.com>
2022-09-04 16:38:40 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
 
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
 const apiKey = ",&appid=d24bf70d6dae818a6893be61edd0ae3c&units=metric";
@@ -25,12 +25,14 @@ const generateData = () => {
         feelings,
       };
 
-      postData(server + "/add", info);
+      postData(server + "/add", info).then(function () {
+        updatingUI()
+      })
 
-      updatingUI();
+      
       document.getElementById('entry').style.opacity = 1;
     }
-  });
+  })
 };
 
 document.getElementById("generate").addEventListener("click", generateData);
